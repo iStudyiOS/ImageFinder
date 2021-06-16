7544f3859173d7debf15b380cd066d4163ffe5a6
AlphaAOmega <france998811@gmail.com>
2021-06-16 23:40:54 +0900

    Create a MVC project

---
+++ ImageFinder_MVC/ImageFinder_MVC/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ImageFinder_MVC
+//
+//  Created by 하동훈 on 2021/06/16.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
